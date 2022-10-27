4f588ed05f2944d6b3bd232a3e637ab53ab8b710
A.J. Bobo <aj.bobo@niceincontact.com>
2022-10-27 17:46:27 -0600

    Initial working version

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NETVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string version = RuntimeInformation.FrameworkDescription;
+            Console.WriteLine($"This is running in {version}");
+
+            // This is required when using Visual Studio 2019 - VS Code redirects output, so this is not required there
+            // Console.Write("Press any key to continue...");
+            // Console.ReadKey();
+        }
+    }
+}
